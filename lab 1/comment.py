cce8f5ee58cc0b1bd61cc7528f15dd617444714e
KeanuRiwz <qian@mail.com>
2024-01-23 18:51:23 +0600

    1st

---
+++ lab 1/comment.py
@@ -0,0 +1,10 @@
+# ex 1
+#This is comment
+
+#ex 2
+
+'''
+This is a comment
+written in 
+more than just one line
+'''
--- lab 1/syntax.py
+++ lab 1/syntax.py
@@ -1,2 +1,6 @@
 # ex 1
 print("Hello world")
+
+# ex 2
+if 5 > 2:
+    print("YES")
